dcfd0219d0d10713cbfb9f3cb02c0079bb3f5b21
wentsingnee <8090395+wentsingnee@user.noreply.gitee.com>
2024-02-28 20:01:20 +0800

    custom market1501

--- torchreid/data/datasets/image/market1501.py
+++ torchreid/data/datasets/image/market1501.py
@@ -87,7 +87,7 @@ class Market1501(ImageDataset):
             pid, camid = map(int, search_res.groups())
             if pid == -1:
                 continue # junk images are just ignored
-            assert 0 <= pid <= 1501 # pid == 0 means background
+            assert 0 <= pid <= 150001 # pid == 0 means background
             assert 1 <= camid <= 6
             camid -= 1 # index starts from 0
             if relabel:
