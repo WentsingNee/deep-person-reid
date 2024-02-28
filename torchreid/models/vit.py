aa439a298701ff044db56bc8a378231d52dfc488
wentsingnee <8090395+wentsingnee@user.noreply.gitee.com>
2024-02-28 21:21:22 +0800

    vit

--- torchreid/models/vit.py
+++ torchreid/models/vit.py
@@ -5,7 +5,16 @@ __all__ = [
     'vit'
 ]
 
+
+class InitPretrainedWeights:
+
+    def __int__(self):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        pass
+
 def vit(num_classes=1000, **kwargs):
     # Load pre-trained ViT
     vit_base = timm.create_model('vit_base_patch16_224', pretrained=True, num_classes=num_classes)
-    return vit_base
+    return vit_base, InitPretrainedWeights()
